a24b373dc61961d98007bc506e380451c69d87d4
Jorge Bravo Cordoba <jorge.bravo@globant.com>
2017-05-10 13:57:36 -0300

    Modificar cocktail uri

--- services/cocktailService.js
+++ services/cocktailService.js
@@ -2,12 +2,8 @@ app.service('cocktailService',['$http', function($http) {
 
 	var listCocktails = function(){
 		console.log("List cocktails");
-		var auth = window.btoa("Jorge:123");
-	    var headers = {"Authorization": "Basic " + auth};
-	    console.log("-------------- JSON: " + auth)
-
 		// return $http.get('https://cocktail-jebc.herokuapp.com/cocktails/', {headers: headers});
-		return $http.get('http://35.188.104.70:8080/cocktail-1.0-SNAPSHOT/cocktails/', {headers: headers});
+		return $http.get('http://35.188.104.70:8080/cocktail-1.0-SNAPSHOT/cocktails/');
 	};
 
 	var saveCocktail = function(cocktail){
